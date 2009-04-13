07b2f71438d4c47cf0ff3eff0b3e7b95a11459d5
Joe Yang <joe.yangyi@gmail.com>
2009-04-14 00:21:00 +0800

    remove i18n dependency

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,6 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :lib => "will_paginate", 
     :source => "http://gems.github.com"
 
-  config.gem 'svenfuchs-i18n', :lib => "i18n", :source => "http://gems.github.com"
   # Skip frameworks you're not going to use (only works if using vendor/rails).
   # To use Rails without a database, you must remove the Active Record framework
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
